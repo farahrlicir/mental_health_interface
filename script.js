72e2283abb9a146436b2fbc84af2092989eb4a0c
farahrlicir <farahlicir15@gmail.com>
2025-05-09 01:05:12 +0200

    second commit

---
+++ script.js
@@ -0,0 +1,6 @@
+
+ window.onload = function () {
+            // Hide loader and display the content once the page is fully loaded
+            document.getElementById('loader').style.display = 'none'; // Hide loader
+            document.getElementById('content').style.display = 'block'; // Show content
+};
