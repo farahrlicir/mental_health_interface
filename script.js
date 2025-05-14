7d87c86ab0f71dfd4b6da50056e6802421651a86
farahrlicir <farahlicir15@gmail.com>
2025-05-14 21:33:10 +0200

    first commit

--- script.js
+++ script.js
@@ -4,3 +4,10 @@
             document.getElementById('loader').style.display = 'none'; // Hide loader
             document.getElementById('content').style.display = 'block'; // Show content
 };
+
+const menuButton = document.getElementById('menu-button');
+        const mobileMenu = document.getElementById('mobile-menu');
+
+        menuButton.addEventListener('click', () => {
+            mobileMenu.classList.toggle('hidden');
+        });
